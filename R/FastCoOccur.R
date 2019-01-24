2ac499445c1ab3124436c7f595abd2747985a47f
Schuyler Smith <sdsmith@iastate.edu>
2019-01-24 12:57:37 -0600

    add warning message for Co-occur memory handling

--- R/FastCoOccur.R
+++ R/FastCoOccur.R
@@ -31,6 +31,22 @@ FastCoOccur <- function(phyloseq_obj, treatment, p = 0.05, cores = 0){
 
   treatments <- as.character(unique(phyloseq_obj@sam_data[[treatment_name]]))
   treatment_indices <- lapply(treatments, FUN = function(trt){which(as.character(phyloseq_obj@sam_data[[treatment_name]]) %in% trt)-1})
+
+  ###RAM check .. because apparently some people have 36,000 genes in their studies...
+  n <- nrow(phyloseq_obj@otu_table)
+  memory_function <- function(n, t, r){(6.780e-07 + (2.867e-08 * (n*(n-1)*t))) - r}
+  required_memory <- round((6.780e-07 + (2.867e-08 * (n*(n-1)*length(treatments)))), 2)
+  available_memory <- round(as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo", intern = TRUE))/(1024^2), 2)
+  if(required_memory >= available_memory){
+    message("Calculating ", (n*(n-1)*length(treatments)), " pairs-wise co-occurences.
+ This may take ", required_memory, " GB of RAM, or more.
+ This machine has ", available_memory, " GB of RAM available.
+ Recommend subsetting from ", n, " taxa to ",
+   floor(uniroot(memory_function, t=length(treatments), r=available_memory, lower=0, upper=1000000)$root), ".
+ ... Until I decide a practical way to fix this.."
+    )
+  }
+
   if(cores == 0){cores <- parallel::detectCores()}
   cooccurrence <- FastCoOccur_Rcpp(phyloseq_obj@otu_table, treatment_indices = treatment_indices, treatment_names = treatments, p_cutoff = p, ncores = cores)
   return(as.data.table(cooccurrence))
