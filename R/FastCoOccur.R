70c960fede8a07ed4cdf7c37ffbcc7b8694d0c12
Schuyler Smith <sdsmith@iastate.edu>
2019-01-28 11:36:14 -0600

    change cooccur RAM estimate

--- R/FastCoOccur.R
+++ R/FastCoOccur.R
@@ -25,7 +25,7 @@ FastCoOccur <- function(phyloseq_obj, treatment, p = 0.05, cores = 0){
   # phyloseq_obj = mock_phyloseq; treatment = c("treatment", "day"); p = 0.05
   options(warnings=-1)
 
-  phyloseq_obj <- combine_treatments(phyloseq_obj, treatment)
+  phyloseq_obj <- find_generalists(phyloseq_obj, treatment = treatment)
   if(is.numeric(treatment)){treatment <- colnames(phyloseq_obj@sam_data[,treatment])}
   treatment_name <- paste(treatment, collapse = ".")
 
@@ -35,20 +35,18 @@ FastCoOccur <- function(phyloseq_obj, treatment, p = 0.05, cores = 0){
   ###RAM check .. because apparently some people have 36,000 genes in their studies...
   n <- nrow(phyloseq_obj@otu_table)
   memory_function <- function(n, t, r){(6.780e-07 + (2.867e-08 * (n*(n-1)*t))) - r}
-  required_memory <- round((6.780e-07 + (2.867e-08 * (n*(n-1)*length(treatments)))), 2)
+  required_memory <- round((6.780e-07 + (2.867e-08 * (n*(n-1)*length(treatments))))*p, 2)
   if(Sys.info()['sysname'] == "Windows"){
     available_memory <- round(as.numeric(gsub("\r","",gsub("FreePhysicalMemory=","",system('wmic OS get FreePhysicalMemory /Value',intern=TRUE)[3])))/(1024^2), 2)
     } else {
     available_memory <- round(as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo", intern = TRUE))/(1024^2), 2)}
   if(required_memory >= available_memory){
     message("Calculating ", (n*(n-1)*length(treatments)), " pairs-wise co-occurrences.
- This may take ", required_memory, " GB of RAM, or more.
+ This could take ", required_memory, " GB of RAM, or more depending on the number of significant interactions.
  This machine has ", available_memory, " GB of RAM available.
  Recommend subsetting from ", n, " taxa to ",
-   floor(uniroot(memory_function, t=length(treatments), r=available_memory, lower=0, upper=1000000)$root), ".
- ... Until I decide a practical way to fix this.."
-    )
-  }
+   floor(uniroot(memory_function, t=length(treatments), r=available_memory, lower=0, upper=1000000)$root), " if your systems gets stuck."
+   )}
 
   if(cores == 0){cores <- parallel::detectCores()}
   cooccurrence <- FastCoOccur_Rcpp(phyloseq_obj@otu_table, treatment_indices = treatment_indices, treatment_names = treatments, p_cutoff = p, ncores = cores)
--- R/find_phyloseq_generalists.R
+++ R/find_phyloseq_generalists.R
@@ -2,10 +2,10 @@
 #'
 #' This function takes a phyloseq object and finds which taxa are seen in a given proportion of samples, either in the entire dataset, by treatment, or a particular treatment of interest.
 #' @useDynLib phylosmith
-#' @usage find_generalists(phyloseq_obj, frequency = 0, treatment = NULL, subset = NULL, below = FALSE, drop_samples = FALSE)
+#' @usage find_generalists(phyloseq_obj, treatment = NULL, frequency = 0, subset = NULL, below = FALSE, drop_samples = FALSE)
 #' @param phyloseq_obj A \code{\link[phyloseq]{phyloseq-class}} object created with the \link[=phyloseq]{phyloseq} package.
-#' @param frequency The minimum proportion of samples the taxa is seen in.
 #' @param treatment Column name or number, or vector of, in the \code{\link[phyloseq:sample_data]{sample_data}}. Function then checks if taxa seen frequency number of times in each treatment. If multiple \code{sample_data} columns are given, they will be appended to the \code{sample_data} as one column with '.' separating each.
+#' @param frequency The minimum proportion of samples the taxa is seen in.
 #' @param subset If taxa not needed to be seen in all \code{treatment}, then can check only one particular treatment subset, this works for multiple treatment inputs.
 #' @param below Does frequency define the minimum or maximum, should the presence fall below frequency or not.
 #' @param drop_samples Should the function remove samples that that are empty after removing taxa filtered by frequency.
@@ -23,7 +23,7 @@
 #' find_generalists(mock_phyloseq, frequency = 0.3, treatment = c("day", "treatment"), subset = c("5","soil"))
 #' find_generalists(mock_phyloseq, frequency = 0.3, treatment = c("day", "treatment"), subset = "5.soil")
 
-find_generalists <- function(phyloseq_obj, frequency = 0, treatment = NULL, subset = NULL, below = FALSE, drop_samples = FALSE){
+find_generalists <- function(phyloseq_obj, treatment = NULL, frequency = 0, subset = NULL, below = FALSE, drop_samples = FALSE){
   # data("mock_phyloseq")
   # phyloseq_obj = mock_phyloseq; frequency = 0; treatment = c("treatment", "day"); subset = "5"; below = FALSE; drop_samples = FALSE
   if(!(is.null(treatment))){
