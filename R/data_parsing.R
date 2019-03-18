86d52c60e9c9b834cbda08b499ca12f6afd8a060
sdsmith1390 <sdsmith@iastate.edu>
2019-03-18 17:12:00 -0500

    rename find_x_taxa to without find [ci skip]

--- R/data_parsing.R
+++ R/data_parsing.R
@@ -1,4 +1,45 @@
-# phyloseq_obj = soil_column; treatment = c('Matrix', 'Treatment'); merge_on = treatment; sep = '_'
+#' Find taxa shared between treatments of a phyloseq object. Function from the phylosmith-package.
+#'
+#' Takes a \code{\link[phyloseq]{phyloseq-class}} object and finds which taxa are shared between all of the specified treatments.
+#' @useDynLib phylosmith
+#' @usage common_taxa(phyloseq_obj, treatment, subset = NULL, n = 'all')
+#' @param phyloseq_obj A \code{\link[phyloseq]{phyloseq-class}} object. It must contain \code{\link[phyloseq:sample_data]{sample_data()}}) with information about each sample, and it must contain \code{\link[phyloseq:tax_table]{tax_table()}}) with information about each taxa/gene.
+#' @param treatment Column name as a \code{string} or \code{numeric} in the \code{\link[phyloseq:sample_data]{sample_data}}. This can be a vector of multiple columns and they will be combined into a new column.
+#' @param subset A factor within the \code{treatment}. This will remove any samples that to not contain this factor. This can be a vector of multiple factors to subset on.
+#' @param n Number of treatment groups that need to share the taxa to be considered a common taxa.
+#' @keywords manip
+#' @seealso \code{\link{unique_taxa}}
+#' @export
+
+common_taxa <- function(phyloseq_obj, treatment, subset = NULL, n = 'all'){
+  if(!inherits(phyloseq_obj, "phyloseq")){
+    stop("common_taxa(): `phyloseq_obj` must be a phyloseq-class object", call. = FALSE)
+  }
+  if(is.null(phyloseq_obj@sam_data)){
+    stop("common_taxa(): `phyloseq_obj` must contain sample_data() information", call. = FALSE)
+  }
+  treatment <- check_numeric_treatment(phyloseq_obj, treatment)
+  if(any(!(treatment %in% colnames(phyloseq_obj@sam_data)))){
+    stop("common_taxa(): `treatment` must be at least one column name, or index, from the sample_data()", call. = FALSE)
+  }
+  if(!(is.numeric(n)) & n != 'all'){
+    stop("common_taxa(): `n` must be either 'all' or a numeric value less than the number of treatments being compared", call. = FALSE)
+  }
+  phyloseq_obj <- phyloseq(phyloseq_obj@otu_table, phyloseq_obj@tax_table, phyloseq_obj@sam_data)
+  phyloseq_obj <- taxa_filter(phyloseq_obj, treatment, subset = subset)
+  treatment_name <- paste(treatment, collapse = sep)
+  treatment_classes <- unique(phyloseq_obj@sam_data[[treatment_name]])
+  if(n == 'all'){n <- length(treatment_classes)}
+  if(n > length(treatment_classes)){
+    stop("common_taxa(): `n` must be either 'all' or a numeric value less than the number of treatments being compared", call. = FALSE)
+  }
+  seen_taxa <- lapply(treatment_classes, FUN = function(trt){
+    taxa_names(taxa_filter(phyloseq_obj, treatment = treatment_name, subset = trt, frequency = 0))
+  }); names(seen_taxa) <- treatment_classes
+  taxa_counts <- table(unlist(seen_taxa))
+  shared_taxa <- names(taxa_counts[taxa_counts == round(n)])
+  return(shared_taxa)
+}
 
 #' Merge samples based on common factor within sample_data. Function from the phylosmith-package.
 #'
@@ -125,89 +166,6 @@ conglomerate_taxa <- function(phyloseq_obj, classification, hierarchical = TRUE)
   if(!(is.logical(phylo_tree))){phyloseq_obj@phy_tree <- phylo_tree}
   if(!(is.logical(refseq))){phyloseq_obj@refseq <- refseq}
   return(phyloseq_obj)
-}
-
-#' Find taxa shared between treatments of a phyloseq object. Function from the phylosmith-package.
-#'
-#' Takes a \code{\link[phyloseq]{phyloseq-class}} object and finds which taxa are shared between all of the specified treatments.
-#' @useDynLib phylosmith
-#' @usage find_common_taxa(phyloseq_obj, treatment, subset = NULL, n = 'all')
-#' @param phyloseq_obj A \code{\link[phyloseq]{phyloseq-class}} object. It must contain \code{\link[phyloseq:sample_data]{sample_data()}}) with information about each sample, and it must contain \code{\link[phyloseq:tax_table]{tax_table()}}) with information about each taxa/gene.
-#' @param treatment Column name as a \code{string} or \code{numeric} in the \code{\link[phyloseq:sample_data]{sample_data}}. This can be a vector of multiple columns and they will be combined into a new column.
-#' @param subset A factor within the \code{treatment}. This will remove any samples that to not contain this factor. This can be a vector of multiple factors to subset on.
-#' @param n Number of treatment groups that need to share the taxa to be considered a common taxa.
-#' @keywords manip
-#' @seealso \code{\link{find_unique_taxa}}
-#' @export
-
-find_common_taxa <- function(phyloseq_obj, treatment, subset = NULL, n = 'all'){
-  if(!inherits(phyloseq_obj, "phyloseq")){
-    stop("find_common_taxa(): `phyloseq_obj` must be a phyloseq-class object", call. = FALSE)
-  }
-  if(is.null(phyloseq_obj@sam_data)){
-    stop("find_common_taxa(): `phyloseq_obj` must contain sample_data() information", call. = FALSE)
-  }
-  treatment <- check_numeric_treatment(phyloseq_obj, treatment)
-  if(any(!(treatment %in% colnames(phyloseq_obj@sam_data)))){
-    stop("find_common_taxa(): `treatment` must be at least one column name, or index, from the sample_data()", call. = FALSE)
-  }
-  if(!(is.numeric(n)) & n != 'all'){
-    stop("find_common_taxa(): `n` must be either 'all' or a numeric value less than the number of treatments being compared", call. = FALSE)
-  }
-  phyloseq_obj <- phyloseq(phyloseq_obj@otu_table, phyloseq_obj@tax_table, phyloseq_obj@sam_data)
-  phyloseq_obj <- taxa_filter(phyloseq_obj, treatment, subset = subset)
-  treatment_name <- paste(treatment, collapse = sep)
-  treatment_classes <- unique(phyloseq_obj@sam_data[[treatment_name]])
-  if(n == 'all'){n <- length(treatment_classes)}
-  if(n > length(treatment_classes)){
-    stop("find_common_taxa(): `n` must be either 'all' or a numeric value less than the number of treatments being compared", call. = FALSE)
-  }
-  seen_taxa <- lapply(treatment_classes, FUN = function(trt){
-    taxa_names(taxa_filter(phyloseq_obj, treatment = treatment_name, subset = trt, frequency = 0))
-  }); names(seen_taxa) <- treatment_classes
-  taxa_counts <- table(unlist(seen_taxa))
-  shared_taxa <- names(taxa_counts[taxa_counts == round(n)])
-  return(shared_taxa)
-}
-
-#' Find unique taxa between treatments of a phyloseq object. Function from the phylosmith-package.
-#'
-#' This function takes a \code{\link[phyloseq]{phyloseq-class}} object and finds which taxa are taxa that are unique to a specific subset of the data.
-#' @useDynLib phylosmith
-#' @usage find_unique_taxa(phyloseq_obj, treatment, subset = NULL)
-#' @param phyloseq_obj A \code{\link[phyloseq]{phyloseq-class}} object. It must contain \code{\link[phyloseq:sample_data]{sample_data()}}) with information about each sample, and it must contain \code{\link[phyloseq:tax_table]{tax_table()}}) with information about each taxa/gene.
-#' @param treatment Column name as a \code{string} or \code{numeric} in the \code{\link[phyloseq:sample_data]{sample_data}}. This can be a vector of multiple columns and they will be combined into a new column.
-#' @param subset A factor within the \code{treatment}. This will remove any samples that to not contain this factor. This can be a vector of multiple factors to subset on.
-#' @keywords manip
-#' @seealso \code{\link{find_common_taxa}}
-#' @export
-
-
-find_unique_taxa <- function(phyloseq_obj, treatment, subset = NULL){
-  if(!inherits(phyloseq_obj, "phyloseq")){
-    stop("find_unique_taxa(): `phyloseq_obj` must be a phyloseq-class object", call. = FALSE)
-  }
-  if(is.null(phyloseq_obj@sam_data)){
-    stop("find_unique_taxa(): `phyloseq_obj` must contain sample_data() information", call. = FALSE)
-  }
-  treatment <- check_numeric_treatment(phyloseq_obj, treatment)
-  if(any(!(treatment %in% colnames(phyloseq_obj@sam_data)))){
-    stop("find_unique_taxa(): `treatment` must be at least one column name, or index, from the sample_data()", call. = FALSE)
-  }
-  phyloseq_obj <- phyloseq(phyloseq_obj@otu_table, phyloseq_obj@tax_table, phyloseq_obj@sam_data)
-  phyloseq_obj <- taxa_filter(phyloseq_obj, treatment, subset = subset)
-  treatment_name <- paste(treatment, collapse = sep)
-  treatment_classes <- unique(phyloseq_obj@sam_data[[treatment_name]])
-
-  seen_taxa <- lapply(treatment_classes, FUN = function(trt){
-    taxa_names(taxa_filter(phyloseq_obj, treatment = treatment_name, subset = trt, frequency = 0))
-  }); names(seen_taxa) <- treatment_classes
-  taxa_counts <- table(unlist(seen_taxa))
-  unique_taxa <- names(taxa_counts[taxa_counts == 1])
-  unique_taxa <- lapply(seen_taxa, FUN = function(toi){
-    toi[toi %in% unique_taxa]
-  })
-  return(unique_taxa)
 }
 
 #' Melt a phyloseq object into a data.table. Function from the phylosmith-package.
@@ -464,4 +422,43 @@ taxa_proportions <- function(phyloseq_obj, classification, treatment = NA){
     class_table <- class_table[, Proportion := round(Abundance/sum(Abundance),3)][,-2]
   }
   return(class_table)
+}
+
+#' Find unique taxa between treatments of a phyloseq object. Function from the phylosmith-package.
+#'
+#' This function takes a \code{\link[phyloseq]{phyloseq-class}} object and finds which taxa are taxa that are unique to a specific subset of the data.
+#' @useDynLib phylosmith
+#' @usage unique_taxa(phyloseq_obj, treatment, subset = NULL)
+#' @param phyloseq_obj A \code{\link[phyloseq]{phyloseq-class}} object. It must contain \code{\link[phyloseq:sample_data]{sample_data()}}) with information about each sample, and it must contain \code{\link[phyloseq:tax_table]{tax_table()}}) with information about each taxa/gene.
+#' @param treatment Column name as a \code{string} or \code{numeric} in the \code{\link[phyloseq:sample_data]{sample_data}}. This can be a vector of multiple columns and they will be combined into a new column.
+#' @param subset A factor within the \code{treatment}. This will remove any samples that to not contain this factor. This can be a vector of multiple factors to subset on.
+#' @keywords manip
+#' @seealso \code{\link{common_taxa}}
+#' @export
+
+unique_taxa <- function(phyloseq_obj, treatment, subset = NULL){
+  if(!inherits(phyloseq_obj, "phyloseq")){
+    stop("unique_taxa(): `phyloseq_obj` must be a phyloseq-class object", call. = FALSE)
+  }
+  if(is.null(phyloseq_obj@sam_data)){
+    stop("unique_taxa(): `phyloseq_obj` must contain sample_data() information", call. = FALSE)
+  }
+  treatment <- check_numeric_treatment(phyloseq_obj, treatment)
+  if(any(!(treatment %in% colnames(phyloseq_obj@sam_data)))){
+    stop("unique_taxa(): `treatment` must be at least one column name, or index, from the sample_data()", call. = FALSE)
+  }
+  phyloseq_obj <- phyloseq(phyloseq_obj@otu_table, phyloseq_obj@tax_table, phyloseq_obj@sam_data)
+  phyloseq_obj <- taxa_filter(phyloseq_obj, treatment, subset = subset)
+  treatment_name <- paste(treatment, collapse = sep)
+  treatment_classes <- unique(phyloseq_obj@sam_data[[treatment_name]])
+
+  seen_taxa <- lapply(treatment_classes, FUN = function(trt){
+    taxa_names(taxa_filter(phyloseq_obj, treatment = treatment_name, subset = trt, frequency = 0))
+  }); names(seen_taxa) <- treatment_classes
+  taxa_counts <- table(unlist(seen_taxa))
+  unique_taxa <- names(taxa_counts[taxa_counts == 1])
+  unique_taxa <- lapply(seen_taxa, FUN = function(toi){
+    toi[toi %in% unique_taxa]
+  })
+  return(unique_taxa)
 }
--- README.md
+++ README.md
@@ -23,16 +23,16 @@ library(phylosmith)
 
 Call			     | Description
 -------------------- | ------------------------------------------------------------
+[common_taxa](https://schuyler-smith.github.io/phylosmith/data_parsing.html#common_taxa) | find taxa common to each treatment
 [conglomerate_samples](https://schuyler-smith.github.io/phylosmith/data_parsing.html#conglomerate_samples)   |   Merge samples based on common factor within sample_data
 [conglomerate_taxa](https://schuyler-smith.github.io/phylosmith/data_parsing.html#conglomerate_taxa)  |  conglomerate taxa by sample on a given classification level
-[find_common_taxa](https://schuyler-smith.github.io/phylosmith/data_parsing.html#find_common_taxa) | find taxa common to each treatment
-[find_unique_taxa](https://schuyler-smith.github.io/phylosmith/data_parsing.html#find_unique_taxa) | find taxa unique to each treatment
 [melt_phyloseq](https://schuyler-smith.github.io/phylosmith/data_parsing.html#melt_phyloseq)   |   Melt a phyloseq object into a data.table.
 [merge_treatments](https://schuyler-smith.github.io/phylosmith/data_parsing.html#merge_treatments) | combines multiple columns in meta-data into a single column
 [order_treatment](https://schuyler-smith.github.io/phylosmith/data_parsing.html#order_treatment) | sets the orders of the factors in a sample_data column (for ordering graphs)
 [relative_abundance](https://schuyler-smith.github.io/phylosmith/data_parsing.html#relative_abundance) | transform abundance data to relative abundance
 [taxa_proportions](https://schuyler-smith.github.io/phylosmith/data_parsing.html#taxa_proportions) | computes the proportion of a taxa classification
 [taxa_filter](https://schuyler-smith.github.io/phylosmith/data_parsing.html#taxa_filter) | filter taxa by proportion of samples seen in
+[unique_taxa](https://schuyler-smith.github.io/phylosmith/data_parsing.html#unique_taxa) | find taxa unique to each treatment
 
 ### [Graphs](https://schuyler-smith.github.io/phylosmith/graphs.html)
 
